48c0bcd15814d943684f63c1ad34ceede4feb48e
Victor Menezes <victormateus@protonmail.com>
2022-07-29 16:20:43 -0300

    feat: install translations keys using react-intl

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
 import { ThemeProvider } from "styled-components";
 
 import Navbar from "components/Navbar";
@@ -7,19 +9,34 @@ import Layout from "components/Layout";
 import theme from "styles/theme";
 import GlobalStyles from "styles/global.styles";
 
+const languages = {
+  en: require("../locale/en.json"),
+  pt: require("../locale/pt.json"),
+};
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const { locale, defaultLocale } = router;
+  const messages = languages[locale];
+
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <GlobalStyles />
-      <Layout>
-        <Navbar />
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <IntlProvider
+      messages={messages}
+      locale={locale}
+      defaultLocale={defaultLocale}
+    >
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <GlobalStyles />
+        <Layout>
+          <Navbar />
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </IntlProvider>
   );
 }
 
