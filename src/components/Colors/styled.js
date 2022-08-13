7f6ac8f3cfedd350b99f81b63744183154e69428
Victor Menezes <victormateus@protonmail.com>
2022-08-13 16:15:18 -0300

    feat: responsive Style Guide

--- src/components/Colors/styled.js
+++ src/components/Colors/styled.js
@@ -1,4 +1,5 @@
 import styled, { css } from "styled-components";
+import media from "styles/media";
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
@@ -26,26 +27,40 @@ export const Title = styled.h2`
 export const ColorWrapper = styled.div`
   display: flex;
   margin-bottom: ${({ theme }) => theme.spacings.medium};
+  flex-wrap: wrap;
 `;
 
 export const ColorBox = styled.div`
-  width: 128px;
   margin-right: ${({ theme }) => theme.spacings.small};
+  flex: 0 0 33%;
+
+  ${media.tablet`    
+    width: 128px;
+    flex: none;
+  `};
 `;
 
 export const Color = styled.div`
   ${({ hex }) => css`
     background-color: ${hex};
-    height: 128px;
-    width: 100%;
+    height: 5em;
     border: 1px solid;
+
+    ${media.tablet`    
+      height: 128px;
+      width: 100%;
+    `};
   `}
 `;
 
 export const ColorName = styled.h5`
   ${({ theme }) => css`
-    font-size: ${theme.font.sizes.small};
+    font-size: ${theme.font.sizes.xsmall};
     margin: 1rem 0;
+
+    ${media.tablet`    
+      font-size: ${theme.font.sizes.small};
+    `};
   `}
 `;
 
