1c1b69e4843cc207f296c039c52017907e410321
Victor Menezes <victormateus@protonmail.com>
2022-08-21 10:41:13 -0300

    style: add new transition transform

--- src/styles/transitions.js
+++ src/styles/transitions.js
@@ -1,7 +1,9 @@
 const defaultTiming = "0.3s ease-in-out";
 const colorTransition = `color ${defaultTiming}`;
+const transformTransition = `transform ${defaultTiming}`;
 
 const transitions = {
+  TRANSFORM: transformTransition,
   COLOR: colorTransition,
   ALL: defaultTiming,
 };
