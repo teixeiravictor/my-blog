d3dc8b76492f3f8ec372a15a12e81c6812dd817b
Victor Menezes <victormateus@protonmail.com>
2022-08-25 20:27:34 -0300

    feat: create a method to generate RSS feed

---
+++ src/lib/constants.js
@@ -0,0 +1,12 @@
+export const BLOG_TITLE = "Victor Menezes";
+
+export const BLOG_SUBTITLE =
+  "Um blog sobre ciência da computação e ciência. Feito por um entusiasta de matemática, física e programação.";
+
+export const BLOG_DESCRIPTION = [BLOG_TITLE, BLOG_SUBTITLE].join(" | ");
+
+export const BLOG_AUTHOR = "Victor Menezes";
+
+export const BLOG_AUTHOR_LINK = "https://www.linkedin.com/in/menezesvictor/";
+
+export const BLOG_URL = "https://victormenezes.dev";
--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -1,4 +1,8 @@
+import fs from "fs";
+
+import { generateRss } from "lib/generateRSS";
 import { getAllArticles } from "lib/api";
+
 import SectionArticles from "components/SectionArticles";
 
 const Articles = ({ articles }) => <SectionArticles articles={articles} />;
@@ -7,6 +11,11 @@ export default Articles;
 
 export async function getStaticProps({ locales }) {
   const articles = getAllArticles(locales);
+
+  if (process.env.NODE_ENV !== "development") {
+    const rss = await generateRss(articles);
+    fs.writeFileSync("./public/feed.xml", rss);
+  }
 
   return {
     props: {
